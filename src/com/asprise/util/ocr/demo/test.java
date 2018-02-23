7257eb38219f63fe7337342dbdbfc89f714e6d0f
qiansohu <qiansohu@sina.com>
2018-02-23 11:35:18 +0800

    在test.java中减少导入的文件

--- src/com/asprise/util/ocr/demo/test.java
+++ src/com/asprise/util/ocr/demo/test.java
@@ -1,9 +1,8 @@
 /*
- * 新增test 文件用于测试 eagle 20180223
+ * 修改test 文件，减少导入的文件 eagle 20180223
  * 
  */
 package com.asprise.util.ocr.demo;
 
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
