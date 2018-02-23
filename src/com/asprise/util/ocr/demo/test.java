2d7f2be5ff05e5bb2831e9e145f25a0427641cf9
qiansohu <qiansohu@gmail.com>
2018-02-23 11:40:39 +0800

    改变为只导入一个文件

--- src/com/asprise/util/ocr/demo/test.java
+++ src/com/asprise/util/ocr/demo/test.java
@@ -1,8 +1,8 @@
 /*
- * 修改test 文件，减少导入的文件 eagle 20180223
+ * 修改test.java 文件，减少导入的文件 eagle 20180223
  * 
  */
 package com.asprise.util.ocr.demo;
 
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
+
